87e348faac23ea77c8b161b49e89ff96b7e0b43b
root <belenddguez@gmail.com>
2020-03-10 22:15:34 +0000

    crear index html

---
+++ index.js
@@ -0,0 +1,15 @@
+const cool = require("cool-ascii-faces");
+const express = require("express");
+
+var app = express();
+
+//para que podamos usarlo en un html //si hago npm start pero no pongo /cool, me devuelve por defecto el index.html
+app.use("/", express.static("./public"));
+
+app.get("/cool", (request, response) => {
+    response.send("<html>" +cool()+"</html>");
+});
+
+app.listen(80, () => {
+    console.log("Servidor listo ",cool())
+});
