43b35233d064b9ef8b1a825ca304376f7700a2db
root <belenddguez@gmail.com>
2020-03-11 20:02:13 +0000

    integracion heroku

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 
 var app = express();
 
+//variable para el puerto
+var port = process.env.PORT || 80;
+
 //para que podamos usarlo en un html //si hago npm start pero no pongo /cool, me devuelve por defecto el index.html
 app.use("/", express.static("./public"));
 
@@ -10,6 +13,6 @@ app.get("/cool", (request, response) => {
     response.send("<html>" +cool()+"</html>");
 });
 
-app.listen(81, () => {
+app.listen(port, () => {
     console.log("Servidor listo ",cool())
 });
