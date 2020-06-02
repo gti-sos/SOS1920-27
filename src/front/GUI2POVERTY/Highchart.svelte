35d27271e42baf9d877624ad3a9bc22f83fbbbef
JMPere <j_manuel1997@hotmail.com>
2020-06-02 16:32:38 +0200

    adsa

--- src/front/GUI2POVERTY/Highchart.svelte
+++ src/front/GUI2POVERTY/Highchart.svelte
@@ -230,12 +230,11 @@
         });
 
     }
+    
 </script>
 
 <svelte:head>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/series-label.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+
     <script src="https://code.highcharts.com/modules/export-data.js"on:load="{loadGraphs}"></script>
 </svelte:head>
 
